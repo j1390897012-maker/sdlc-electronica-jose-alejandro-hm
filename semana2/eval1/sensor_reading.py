584dc2d94587c3e531dcab7576063b4981ef6d3f
Jose Alejandro HM <j1390897012@gmail.com>
2026-07-24 16:41:43 -0600

    refactor: convertir SensorReading en dataclass

--- semana2/eval1/sensor_reading.py
+++ semana2/eval1/sensor_reading.py
@@ -1,14 +1,7 @@
+from dataclasses import dataclass
 
-
+@dataclass
 class SensorReading:
-    def __init__(
-        self, 
-        sensor_id: str,
-         value: float,
-          timestamp:str) -> None:
-        self.sensor_id = sensor_id
-        self.value = value
-        self.timestamp = timestamp
-        
-
-
+    sensor_id: str
+    value: float
+    timestamp: str
